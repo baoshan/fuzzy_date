17d142b41fe38452901f821cbebb31a3efbe0277
Baoshan Sheng <sheng@icmd.org>
2014-08-10 23:02:03 +0800

    C# CLR Implementation

---
+++ ICMD.DB.CLR/ICMD.DB.CLR.Test/fuzzy_date/fuzzy_date_test.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.SqlTypes;
+using ICMD.DB.CLR;
+
+namespace ICMD.DB.CLR.Test
+{
+  [TestClass]
+  public partial class FuzzyDateTest
+  {
+    /// <summary>
+    /// Negative examples which must fail.
+    /// </summary>
+    [TestMethod]
+    public void InvalidFuzzyDateStrings()
+    {
+      string[] negative_examples = {
+        "d-0",
+        "d-13",
+        "d-12-32",
+        "d-2-0",
+        "d-2-30",
+        "11sBC",
+        "2001-2-29",
+        "1025BC",
+        "3072",
+        "0",
+        "0-1",
+        "0-1-1",
+        "2011s",
+        "d-2-30",
+        "2014+0",
+        "2014+33",
+        "2020s+1",
+        "2020s+110"
+      };
+      foreach (var negative_example in negative_examples)
+      {
+        Assert.IsTrue(UDFs.fuzzy_date_binary_from_string(negative_example).IsNull);
+      }
+    }
+
+    /// <summary>
+    /// Positive examples which should pass. Examples are given in ascending order.
+    /// </summary>
+    [TestMethod]
+    public void ValidFuzzyDateStrings()
+    {
+      string[,] positive_examples = { 
+        {"d-12", "December"},
+        {"d-12-25","December 25"},
+        {"1024BC", "1024 BC"},
+        {"212BC", "212 BC"},
+        {"212BC-8", "August 212 BC"},
+        {"212BC-8-6", "August 6, 212 BC"},
+        {"?24BC", "? 24 BC"},
+        {"?c.20BC", "? c. 20 BC"},
+        {"10sBC+10", "10s BC – 0s BC"},
+        {"10sBC+20", "10s BC – 0s AD"},
+        {"0sBC", "0s BC"},
+        {"0sBC+10", "0s BC – 0s AD"},
+        {"c.9BC+2", "c. 9 BC – 7 BC"},
+        {"c.9BC+20", "c. 9 BC – 12 AD"},
+        {"1BC", "1 BC"},
+        {"1BC+1", "1 BC – 1 AD"},
+        {"0s", "0s"},
+        {"1", "1"},
+        {"2010s", "2010s"},
+        {"2010+10", "2010 – 2020"},
+        {"fl.?c.2014", "fl. ? c. 2014"},
+        {"?c.2014", "? c. 2014"},
+        {"c.2014", "c. 2014"},
+        {"2014", "2014"},
+        {"2014-8", "August 2014"},
+        {"2014-8-6", "August 6, 2014"},
+        {"2014+1", "2014 – 2015"},
+        {"2020s+20", "2020s – 2040s"},
+        {"3071", "3071"}
+      };
+      var prev_valid_binary = SqlBinary.Null;
+      for (var i = 0; i < positive_examples.GetLength(0); i++ )
+      {
+        var valid_binary = UDFs.fuzzy_date_binary_from_string(positive_examples[i, 0]);
+        Assert.IsFalse(valid_binary.IsNull);
+        Assert.AreEqual(positive_examples[i, 0], UDFs.fuzzy_date_string_from_binary(valid_binary));
+        Assert.AreEqual(positive_examples[i, 1], UDFs.fuzzy_date_readable_string_from_binary(valid_binary));
+        if (!prev_valid_binary.IsNull) { Assert.IsTrue((valid_binary > prev_valid_binary).Value); }
+        prev_valid_binary = valid_binary;
+      }
+    }
+  }
+}
---
+++ ICMD.DB.CLR/ICMD.DB.CLR/fuzzy_date/fuzzy_date_binary_from_string.cs
@@ -0,0 +1,122 @@
+using Microsoft.SqlServer.Server;
+using System.Data.SqlTypes;
+using System.Text.RegularExpressions;
+
+namespace ICMD.DB.CLR
+{
+  public partial class UDFs
+  {
+    // Compiled static regex to separate digits.
+    static readonly Regex regex = new Regex(@"\d+", RegexOptions.Compiled);
+
+    /// <summary>
+    /// Parse a short string into a fuzzy date binary.
+    /// </summary>
+    [SqlFunction(IsDeterministic = true, IsPrecise = true)]
+    public static SqlBinary fuzzy_date_binary_from_string(SqlString sql_string)
+    {
+      // NULL in NULL out.
+      if (sql_string.IsNull) { return SqlBinary.Null; }
+
+      // Any exception due to invalid input leads to SqlBytes.Null be returned.
+      try
+      {
+        // Get the input string (w/o whitespaces in lowercase).
+        var input = sql_string.Value.Replace(" ", "").ToLower();
+
+        // Initialize the output bytes.
+        byte[] bytes = { 0, 0, 0 };
+
+        // Search the string for all continuous digits.
+        var matches = regex.Matches(input);
+
+        // When the string does not have a year part:
+        if (input.Contains("d"))
+        {
+          // 1. Calculate the month bits.
+          // 2. Calculate the day bits if the string contains a day part.
+          bytes[1] = (byte)(byte.Parse(matches[0].Value) + 1);
+          if (matches.Count > 1) { bytes[2] = (byte)(byte.Parse(matches[1].Value) << 3); }
+        }
+
+        // When the string has a year part:
+        else
+        {
+          // Parse the year part.
+          var year = int.Parse(matches[0].Value);
+
+          if (input.Contains("bc"))
+          {
+            // Valid BC decades start from:
+            //   * -18 (10s BC starts from 19 BC)
+            //   * -8 (0s BC starts from 9 BC)
+            //   * ...
+            if (input.Contains("s")) { year = -8 - year; }
+
+            // 1 BC before 1 AD
+            else { year = 1 - year; }
+          }
+
+          // 0s starts from 1 AD.
+          else if (year == 0) { year = 1; }
+
+          // Calculate year bits.
+          year += 1024;
+          bytes[0] = (byte)(year >> 4);
+          bytes[1] = (byte)(year << 4);
+
+          if (input.Contains("+"))
+          {
+            // For fuzzy decades spanning multiple years, 1 day bit equals 10 years.
+            if (input.Contains("s"))
+            {
+              bytes[2] = (byte)(byte.Parse(matches[1].Value) / 10 << 3);
+            }
+
+            // For fuzzy dates spanning multiple years:
+            // 1. Month bits are `0b1111`;
+            // 2. Calculate day bits (spanning years).
+            else
+            {
+              bytes[1] |= 0x0F;
+              bytes[2] = (byte)(byte.Parse(matches[1].Value) - 1 << 3);
+            }
+          }
+
+          // For other fuzzy dates that aren't decades:
+          // 1. Calculate month bits;
+          // 2. Calculate day bits.
+          else if (!input.Contains("s"))
+          {
+            bytes[1] |= (byte)(matches.Count > 1 ? (byte.Parse(matches[1].Value) + 1) : 1);
+            bytes[2] = (byte)(matches.Count > 2 ? (byte.Parse(matches[2].Value) << 3) : 0);
+          }
+        }
+
+        // Flag Bit A: Certainty.
+        if (!input.Contains("?")) { bytes[2] |= 0x04; }
+
+        // Flag Bit B: Accuracy.
+        if (!input.Contains("c.")) { bytes[2] |= 0x02; }
+
+        // Flag Bit C: Special flag.
+        if (!input.Contains("f")) { bytes[2] |= 0x01; }
+
+        // Prepare output binary.
+        var sql_binary = new SqlBinary(bytes);
+
+        // Ensure the output a valid fuzzy date binary.
+        if (!fuzzy_date_is_valid_binary(sql_binary)) { return SqlBinary.Null; }
+
+        // Ensure the output is the same as input.
+        if (input.Replace(".", "") != fuzzy_date_string_from_binary(sql_binary).Value.Replace(" ", "").Replace(".", "").ToLower()) { return SqlBinary.Null; }
+
+        // Return the parsed binary.
+        return sql_binary;
+      }
+
+      // The parser returns SqlBytes.Null for all invalid inputs.
+      catch { return SqlBinary.Null; }
+    }
+  }
+}
